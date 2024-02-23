82646b16018db16b068686bdaee3f196560bd8f5
zigzagoon1 <76269524+zigzagoon1@users.noreply.github.com>
2024-02-23 17:11:47 -0500

    update dog prefab and add to scene, add volcano to armageddon scene

---
+++ Assets/Custom Assets/Scripts/Character/Dog.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Dog : Character
+{
+    public override void TakeDamage(GameObject weapon, float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+}
