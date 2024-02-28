6c13b801a178bbdf6c35c5e4f2e9e64a67ab7375
zigzagoon1 <76269524+zigzagoon1@users.noreply.github.com>
2024-02-28 17:32:11 -0500

    remove custom holding error

--- Assets/Custom Assets/Scripts/System/Controls.cs
+++ Assets/Custom Assets/Scripts/System/Controls.cs
@@ -199,7 +199,7 @@ public partial class @Controls: IInputActionCollection2, IDisposable
                     ""name"": """",
                     ""id"": ""ceef1811-56a2-49f6-83cb-72d6ff7c71bc"",
                     ""path"": ""<Keyboard>/j"",
-                    ""interactions"": ""CustomHolding(duration=0.5,pressPoint=0.5)"",
+                    ""interactions"": """",
                     ""processors"": """",
                     ""groups"": ""PC"",
                     ""action"": ""LaunchAttack"",
