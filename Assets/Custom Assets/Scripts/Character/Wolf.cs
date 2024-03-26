4d141e9c286699e1d5aa2f9d495caa7ba1c2650f
zigzagoon1 <76269524+zigzagoon1@users.noreply.github.com>
2024-03-26 15:57:46 -0400

    discarded unused baaaaa's, add audio source to character prefabs, isolate useful dog sounds from recordings

--- Assets/Custom Assets/Scripts/Character/Wolf.cs
+++ Assets/Custom Assets/Scripts/Character/Wolf.cs
@@ -75,7 +75,6 @@ public class Wolf : Enemy
         activeWolves = GameManager.instance.activeWolves;
         activeSheep = GameManager.instance.activeSheep;
         fsm.OnSpawn(_chase);
-        _animator.Play("Run");
 
     }
     void FixedUpdate()
