f7ba15c761982088a1446dcfb86af3964dd9ace5
fernandarobles1234 <fernandarobles1234@gmail.com>
2024-03-31 11:27:29 -0400

    Practice commit has been removed

--- Assets/terrainColliderHack.cs
+++ Assets/terrainColliderHack.cs
@@ -6,7 +6,6 @@ public class terrainColliderHack : MonoBehaviour
 {
     void Awake() 
     {
-        // Commit test - Will remove
         // some dumb forum post mentioned this might fix the problem
         // with tree colliders not working lolololololololololololol
         GetComponent<TerrainCollider>().enabled = false;
